4d671296ff7c1c939c836bacf4e294a1a05a3277
s0berano <tavareslocal@gmail.com>
2020-06-09 08:02:46 -0400

    Added custom firefox template

---
+++ chrome/README.md
@@ -0,0 +1,20 @@
+# Firefox CSS 
+A dark userstyle for Firefox.
+Forked version from [moonlight-userChrome](https://github.com/eduardhojbota/moonlight-userChrome).
+
+## Installation
+1. Open your currently active profile folder ( "about:support" -> "Profile Folder" )
+
+2. Create a new folder named chrome
+
+3. Copy the files in the chrome folder
+
+4. If you're running Firefox 69+ make sure to enable 'toolkit.legacyUserProfileCustomizations.stylesheets' in about:config 
+
+## Additional Features
+
+Additional Features are disabled by default but they can be enabled by uncommenting them in userChrome.css.
+
+## Preview
+
+<img src="images/preview.png">
--- i3/scripts/locker.sh
+++ i3/scripts/locker.sh
@@ -2,13 +2,13 @@
 
 sleep 0.7;
 
-B='#FCFCFC00'  # blank
-C='#30303050'  # clear ish
-D='#303030A0'  # default
-K='#30303090'  # default
-T='#FCFCFCee'  # text
+B='#E78C6D00'  # blank
+C='#623a4350'  # clear ish
+D='#623a43A0'  # default
+K='#623a4390'  # default
+T='#E78C6Dee'  # text
 W='#880000bb'  # wrong
-V='#303030B1'  # verifying
+V='#623a43B1'  # verifying
 
 i3lock \
 --insidevercolor=$D   \
@@ -22,25 +22,24 @@ i3lock \
 --linecolor=$B        \
 --separatorcolor=$V   \
 \
---verifcolor=$T        \
---wrongcolor=$T        \
+--verifcolor=$T       \
+--wrongcolor=$T       \
 --timecolor=$T        \
 --datecolor=$T        \
 --layoutcolor=$T      \
 --keyhlcolor=$T       \
 --bshlcolor=$V        \
 \
---screen 1            \
---blur 2              \
+--screen 0            \
+--blur 10             \
 --clock               \
 --indicator           \
 --timestr="%H:%M:%S"  \
 --datestr="%A, %m %Y" \
 --keylayout 2         \
---veriftext="Wait" \
---wrongtext="Fail" \
-#--textsize=20 \
---modsize=50 \
---timefont=SF Pro Diplay \
---datefont=SF Pro Display \
-# etc
+--veriftext="Wait"    \
+--wrongtext="Fail"    \
+--time-font="SF Pro Diplay"  \
+--date-font="SF Pro Display" \
+--verif-font="SF Pro Display" \
+--wrong-font="SF Pro Display" \
