34d16a8de491ab6fbe78c750a3363007d7d3c1bc
s0berano <tavareslocal@gmail.com>
2020-06-12 09:33:19 -0400

    Changed some configs and added new themes

---
+++ gtk-themes/README.md
@@ -0,0 +1,6 @@
+All this folder files has a soft link with /usr/share/folder, so this makes possible the changes on files and more facility to share.
+
+1 - Over the folder /usr/share/themes/
+
+ln -s ~/.config/gtk-themes/<theme_folder> <link_name>
+
--- i3/scripts/auto_xset.py
+++ i3/scripts/auto_xset.py
@@ -10,6 +10,8 @@ monitor_status = 'xset -q'
 set_monitor_on = 'xset s off'
 set_monitor_off = 'xset dpms 0 0 '
 i3_cmd = 'i3-msg -t get_tree'
+suspend = 'systemctl suspend'
+locker_status = 'pgrep -f i3lock'
 lock_screen = 'sh /home/s0berano/.config/i3/scripts/locker.sh'
 
 def get_layout():
@@ -31,22 +33,41 @@ def command_call(arg):
 
     exit_code = subprocess.call(arg.split(), shell=False)
 
-    print('Screen Saver: {}\nStatus: {}'.format(arg, exit_code))
+    print('Command: {}\nStatus: {}'.format(arg, exit_code))
+
+def get_locker_status():
+    return subprocess.Popen(locker_status.split(), stdout=subprocess.PIPE).communicate()[0].decode('utf-8').replace('\n', '').replace(' ', '')
+
+def get_monitor_status():
+    return subprocess.Popen(monitor_status.split(), stdout=subprocess.PIPE).communicate()[0][-4:].decode('utf-8').replace('\n', '').replace(' ', '')
+
 
 if __name__ == '__main__':
-    sleep_time = 300 #five mintues
+    sleep_time   = 300 #five mintues
+    lock_time    = 15
+    suspend_time = 100
 
     while True:
-        get_monitor_status = subprocess.Popen(monitor_status.split(), stdout=subprocess.PIPE).communicate()[0][-4:].decode('utf-8').replace('\n', '')
-
-        if (any_fullscreen_window(get_layout())):
+        if (any_fullscreen_window(get_layout())) :
             command_call(set_monitor_off + '0')
-        else:
-            command_call(set_monitor_off + str(sleep_time))
-            print('Monitor: ' + get_monitor_status)
-            if(get_monitor_status == 'Off') :
-                time.sleep(5)
-                command_call(lock_screen)
-
-        time.sleep(sleep_time)
         
+        if(get_monitor_status() == 'On' and not any_fullscreen_window(get_layout())) :
+            command_call(set_monitor_off + str(sleep_time))
+
+        print('Monitor: ' + get_monitor_status())
+
+        time.sleep(lock_time)
+
+        if(get_monitor_status() == 'Off' and get_locker_status() == '') :
+            print('Locking...')
+            command_call(lock_screen)
+
+        time.sleep(suspend_time)
+
+        if(get_monitor_status() == 'Off' and get_locker_status() != '') :
+            print('Suspending...')
+            command_call(suspend)
+        
+        time.sleep(sleep_time)
+                
+       
